0595ab56af736ffa061f753a14bce30c76959912
Romario-OQ <57140396+Romario-OQ@users.noreply.github.com>
2019-12-13 10:50:58 -0500

    Update index.php

--- index.php
+++ index.php
@@ -14,46 +14,46 @@
   </head>
   <body>
   
-  <div class="container">
-		<div class="row">
-			<div class="col-sm-12">
-				<h1>Calidad del Agua</h1>
-				<!-- <p></p>-->		
+	  <div class="container">
+			<div class="row">
+				<div class="col-sm-12">
+					<h1>Calidad del Agua</h1>
+					<!-- <p></p>-->		
+				</div>
+		</div>
+
+		<div class="row">	
+			<div class="col-sm-12 col-md-6 col-lg-6">
+
+			<h3>Crear una Cuenta</h3><hr />
+
+			<form method="post" action="create-account.php" method="POST">
+				<div class="form-group">				
+					<input type="text" class="form-control" name="name" placeholder="Introducir nombre" required>			
+			 </div>
+
+				<div class="form-group">				
+					<input type="email" class="form-control" name="email" aria-describedby="emailHelp" placeholder="Introducir email" required>
+				</div>
+
+			  <div class="form-group">				
+					<input type="password" class="form-control" name="password" placeholder="Crear contraseña" required>
+				</div>
+
+			  <button type="submit" class="btn btn-success btn-block">Crear Cuenta</button>
+			</form>		
+			</div>		
+			<div class="col-sm-12 col-md-6 col-lg-6">
+				<h3>Login</h3><hr />
+				<p>Ya tienes una cuenta? <a href="login.php" title="Login Here">Entre aquí!</a></p>
 			</div>
-	</div>
-	
-	<div class="row">	
-		<div class="col-sm-12 col-md-6 col-lg-6">
-		
-		<h3>Crear una Cuenta</h3><hr />
-		
-		<form method="post" action="create-account.php" method="POST">
-			<div class="form-group">				
-				<input type="text" class="form-control" name="name" placeholder="Introducir nombre" required>			
-		  </div>
-		  
-		  <div class="form-group">				
-				<input type="email" class="form-control" name="email" aria-describedby="emailHelp" placeholder="Introducir email" required>
-			</div>
-		  
-		  <div class="form-group">				
-				<input type="password" class="form-control" name="password" placeholder="Crear contraseña" required>
-			</div>
-		  
-		  <button type="submit" class="btn btn-success btn-block">Crear Cuenta</button>
-		</form>		
-		</div>		
-		<div class="col-sm-12 col-md-6 col-lg-6">
-			<h3>Login</h3><hr />
-			<p>Ya tienes una cuenta? <a href="login.php" title="Login Here">Entre aquí!</a></p>
 		</div>
 	</div>
-</div>
     <!-- Optional JavaScript -->
     <!-- jQuery first, then Popper.js, then Bootstrap JS -->
     <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.3/umd/popper.min.js" integrity="sha384-vFJXuSJphROIrBnz7yo7oB41mKfc8JzQZiCq4NCceLEaO4IHwicKwpJf9c9IpFgh" crossorigin="anonymous"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/js/bootstrap.min.js" integrity="sha384-alpBpkh1PFOepccYVYDB4do5UnbKysX5WZXm3XxPqe5iKTfUKjNkCk9SaVuEZflJ" crossorigin="anonymous"></script>
  
-	</body>
+</body>
 </html>
