fd09a43d81d7c0091ee882444daa697492bde05b
Romario-OQ <57140396+Romario-OQ@users.noreply.github.com>
2019-12-13 11:36:06 -0500

    Update conn.php

--- conn.php
+++ conn.php
@@ -2,6 +2,6 @@
 // Connection variables
 $dbhost	= "ec2-174-129-255-72.compute-1.amazonaws.com";	   // localhost or IP
 $dbuser	= "eigrdorbnvbzyj";		  // database username
-$dbpass	= "2289a86143cef2116145b957712905356367848ab3f4e367a2d4930e06b1d5e7";		     // database password
+$dbpass	= "12345";		     // database password
 $dbname	= "d3voea58ggit40";    // database name
 ?>
